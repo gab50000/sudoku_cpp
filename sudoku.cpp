00f44c70625585075fddbc65070f74dcf38e9b38
gk <gabriel.kabbe@mail.de>
2018-03-01 20:27:52 +0100

    Shuffle numbers in first row

--- sudoku.cpp
+++ sudoku.cpp
@@ -2,16 +2,23 @@
 #include <vector>
 #include <random>
 #include <ostream>
+#include <algorithm>
+#include <chrono>
 
 
 #define ROWS 9
 #define COLUMNS 9
 
 
+std::default_random_engine rng;
+
+
 class SudokuBoard{
     public:
-        SudokuBoard(void);
         std::vector<std::vector<int> > board;
+
+        SudokuBoard(void);
+        void insert_first_row();
 };
 
 
@@ -19,8 +26,16 @@ SudokuBoard::SudokuBoard(){
     board.resize(ROWS, std::vector<int>(COLUMNS, 0));
 }
 
+void SudokuBoard::insert_first_row(){
+    for (int i=0; i<board[0].size(); i++){
+        board[0][i] = i + 1;
+    }
+    std::shuffle(board[0].begin(), board[0].end(), rng);
+}
+
 
 std::ostream& operator<<(std::ostream& os, const SudokuBoard& obj){
+    rng.seed(time(0));
     std::string output = "";
     for (const auto& inner: obj.board){
         for (const auto& item: inner){
@@ -33,7 +48,10 @@ std::ostream& operator<<(std::ostream& os, const SudokuBoard& obj){
 
 
 int main(){
+    unsigned seed = std::chrono::system_clock::now().time_since_epoch().count();
+    rng.seed(seed);
     SudokuBoard sudoku;
+    sudoku.insert_first_row();
     std::cout << "Content of Sudoku board:" << std::endl;
     std::cout << sudoku << std::endl;
 }
