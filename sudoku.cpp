de10fe23cc4d11297d5d0b7ee6078fe3b517796d
Gabriel Kabbe <gabriel.kabbe@mail.de>
2020-01-29 21:44:10 +0100

    Rewrite a lot

--- sudoku.cpp
+++ sudoku.cpp
@@ -12,30 +12,44 @@
 
 std::default_random_engine rng;
 
+typedef std::vector<std::vector<int>> GridOfInts;
+typedef std::vector<int> RowOfInts;
 
 class SudokuBoard{
-    public:
-        std::vector<std::vector<int> > board;
+    const GridOfInts board;
 
-        SudokuBoard(void);
-        void insert_first_row();
+    public:
+        SudokuBoard();
+        SudokuBoard(GridOfInts& board);
+        SudokuBoard insert_first_row();
+        SudokuBoard insert_first_row(RowOfInts& row);
+
+    friend std::ostream& operator<<(std::ostream& os, const SudokuBoard& obj);
 };
 
-
-SudokuBoard::SudokuBoard(){
-    board.resize(ROWS, std::vector<int>(COLUMNS, 0));
+SudokuBoard::SudokuBoard() : board(GridOfInts{9, RowOfInts(9, 0)}){};
+SudokuBoard::SudokuBoard(GridOfInts& board) : board {board}{
 }
 
-void SudokuBoard::insert_first_row(){
-    for (int i=0; i<board[0].size(); i++){
-        board[0][i] = i + 1;
+SudokuBoard SudokuBoard::insert_first_row(){
+    RowOfInts row{};
+    for (int i=0; i < COLUMNS; i++){
+        row.push_back(i+1);
     }
-    std::shuffle(board[0].begin(), board[0].end(), rng);
+    std::shuffle(row.begin(), row.end(), rng);
+    std::cout << "hi";
+
+    return insert_first_row(row);
+}
+SudokuBoard SudokuBoard::insert_first_row(RowOfInts& row){
+    auto new_board{board};
+    new_board[0] = row;
+
+    return SudokuBoard{new_board};
 }
 
 
 std::ostream& operator<<(std::ostream& os, const SudokuBoard& obj){
-    rng.seed(time(0));
     std::string output = "";
     for (const auto& inner: obj.board){
         for (const auto& item: inner){
@@ -50,8 +64,14 @@ std::ostream& operator<<(std::ostream& os, const SudokuBoard& obj){
 int main(){
     unsigned seed = std::chrono::system_clock::now().time_since_epoch().count();
     rng.seed(seed);
-    SudokuBoard sudoku;
-    sudoku.insert_first_row();
-    std::cout << "Content of Sudoku board:" << std::endl;
-    std::cout << sudoku << std::endl;
+    // SudokuBoard sudoku;
+    // sudoku.insert_first_row();
+    // std::cout << "Content of Sudoku board:" << std::endl;
+    // std::cout << sudoku << std::endl;
+    std::vector<std::vector<int>> x{9, std::vector<int>(9, 0)};
+
+    SudokuBoard board;
+
+    std::cout << board;
+
 }
