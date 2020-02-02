fc6228a807e04371cf6b78504e8816d92f04127f
Gabriel Kabbe <gabriel.kabbe@mail.de>
2020-02-02 14:34:47 +0100

    Lots of updates

--- sudoku.cpp
+++ sudoku.cpp
@@ -4,65 +4,125 @@
 #include <ostream>
 #include <algorithm>
 #include <chrono>
-
+#include <assert.h>
 
 #define ROWS 9
 #define COLUMNS 9
-
 
 std::default_random_engine rng;
 
 typedef std::vector<std::vector<int>> GridOfInts;
 typedef std::vector<int> RowOfInts;
 
-class SudokuBoard{
-    const GridOfInts board;
+class SudokuBoard
+{
 
-    public:
-        SudokuBoard();
-        SudokuBoard(GridOfInts& board);
-        SudokuBoard insert_first_row();
-        SudokuBoard insert_first_row(RowOfInts& row);
-        SudokuBoard fill_board();
-        SudokuBoard fill_board(int row, int col, int val);
+public:
+    SudokuBoard();
+    SudokuBoard(GridOfInts &board);
+    SudokuBoard insert_first_row();
+    SudokuBoard insert_first_row(RowOfInts &row);
+    SudokuBoard insert(int row, int col, int val);
+    SudokuBoard fill_board();
+    SudokuBoard fill_board(int row, int col, int val);
+    bool is_valid(int row, int col);
 
-    friend std::ostream& operator<<(std::ostream& os, const SudokuBoard& obj);
+    friend std::ostream &operator<<(std::ostream &os, const SudokuBoard &obj);
+
+private:
+    GridOfInts board;
 };
 
 SudokuBoard::SudokuBoard() : board(GridOfInts{9, RowOfInts(9, 0)}){};
-SudokuBoard::SudokuBoard(GridOfInts& board) : board {board}{
+SudokuBoard::SudokuBoard(GridOfInts &board) : board{board}
+{
 }
 
-SudokuBoard SudokuBoard::insert_first_row(){
+SudokuBoard SudokuBoard::insert_first_row()
+{
     RowOfInts row{};
-    for (int i=0; i < COLUMNS; i++){
-        row.push_back(i+1);
+    for (int i = 0; i < COLUMNS; i++)
+    {
+        row.push_back(i + 1);
     }
     std::shuffle(row.begin(), row.end(), rng);
 
     return insert_first_row(row);
 }
 
-SudokuBoard SudokuBoard::insert_first_row(RowOfInts& row){
+SudokuBoard SudokuBoard::insert_first_row(RowOfInts &row)
+{
     auto new_board{board};
     new_board[0] = row;
 
     return SudokuBoard{new_board};
 }
 
-
-SudokuBoard SudokuBoard::fill_board(){
-    return *this;
-}
-SudokuBoard SudokuBoard::fill_board(int row, int col, int val){
-
+SudokuBoard SudokuBoard::fill_board()
+{
+    return SudokuBoard(*this);
 }
 
+SudokuBoard SudokuBoard::fill_board(int row, int col, int val)
+{
+    return SudokuBoard(*this);
+}
 
-std::ostream& operator<<(std::ostream& os, const SudokuBoard& obj){
+bool SudokuBoard::is_valid(int row, int col)
+{
+    int value_to_check = board[row][col];
+
+    assert(value_to_check != 0);
+
+    for (int i = 0; i < 9; i++)
+    {
+        if (i != row)
+        // check for duplicates in the same column
+        {
+            if (board[i][col] == value_to_check)
+            {
+                return false;
+            }
+        }
+
+        if (i != col)
+        {
+            if (board[row][i] == value_to_check)
+            {
+                return false;
+            }
+        }
+    }
+
+    int m = row / 3;
+    int n = col / 3;
+
+    for (int i = 0; i < 3; i++)
+    {
+        for (int j = 0; j < 3; j++)
+        {
+            if (m + i == row && n + j == col)
+            {
+                continue;
+            }
+
+            if (board[m + i][n + j] == value_to_check)
+            {
+                return false;
+            }
+        }
+    }
+
+    return true;
+}
+
+std::ostream &operator<<(std::ostream &os, const SudokuBoard &obj)
+{
     std::string output = "";
-    for (const auto& inner: obj.board){
-        for (const auto& item: inner){
+    for (const auto &inner : obj.board)
+    {
+        for (const auto &item : inner)
+        {
             output += std::to_string(item) + " ";
         }
         output += "\n";
@@ -70,8 +130,16 @@ std::ostream& operator<<(std::ostream& os, const SudokuBoard& obj){
     return os << output;
 }
 
+SudokuBoard SudokuBoard::insert(int row, int col, int val)
+{
+    auto new_board = GridOfInts(board);
+    new_board[row][col] = val;
 
-int main(){
+    return SudokuBoard(new_board);
+}
+
+int main()
+{
     unsigned seed = std::chrono::system_clock::now().time_since_epoch().count();
     rng.seed(seed);
     // SudokuBoard sudoku;
@@ -81,7 +149,10 @@ int main(){
     std::vector<std::vector<int>> x{9, std::vector<int>(9, 0)};
 
     SudokuBoard board;
+    board = board.insert_first_row().fill_board();
 
-    std::cout << board.insert_first_row().fill_board();
+    SudokuBoard board2 = board.insert(1, 0, 9);
 
+    std::cout << board2;
+    std::cout << board2.is_valid(1, 0) << std::endl;
 }
