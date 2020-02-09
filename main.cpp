c159384a91d9658662db0a775ec6a47c566133fb
Gabriel Kabbe <gabriel.kabbe@mail.de>
2020-02-09 11:59:57 +0100

    Add function to parse vector of ints from file

--- main.cpp
+++ main.cpp
@@ -16,12 +16,28 @@ void read_file(const std::string &filename)
     }
 }
 
+std::vector<int> read_ints(const std::string &filename)
+{
+    std::ifstream infile(filename);
+    std::istream_iterator<int> iter(infile);
+    std::istream_iterator<int> endIter;
+
+    std::vector<int> vec{iter, endIter};
+    return vec;
+}
+
 int main(int argc, char **argv)
 {
 
     if (argc > 1)
     {
         std::cout << argv[1] << std::endl;
-        read_file(argv[1]);
+        // read_file(argv[1]);
+        auto vec = read_ints(argv[1]);
+
+        for (const int &x : vec)
+        {
+            std::cout << x << std::endl;
+        }
     }
 }
